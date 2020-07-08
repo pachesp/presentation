9a8d16ad92355ebf9977f99bab86e52899134601
Francisco Espinosa <paco.espinosa.pelaez@gmail.com>
2020-07-08 19:56:13 +0200

    add line division and rectangle

--- Resources/Images/plot_data.m
+++ Resources/Images/plot_data.m
@@ -11,12 +11,14 @@ x = unidirectioanlArr_g0(:, 7);
 
 figure
 plot(x, h_uni_g0, x, h_uni_g)
-axis([600, 1300, 8, 11.5])
+xline(1000, '--', 'LineWidth', 1.8)
+rectangle('Position',[800 8.2 240 1.3],'EdgeColor','r', 'LineWidth', 1.8)
+axis([650, 1150, 8, 10.25])
 grid on
 title('Unidirectional 2D-2D, t=20')
 xlabel('x')
 ylabel('height')
-legend('Gradient = 0', 'Gradient \neq 0');
+legend('Gradient = 0', 'Gradient \neq 0', 'domain division');
 saveas(gcf,'unidirectional_graph.png')
 
 bidirectionalArr_g_0=csvread('bi_data_g0.csv');
@@ -28,12 +30,14 @@ h_bidi_g_bueno = bidirectionalArr_g_bueno(:, 1);
 
 figure
 plot(x, h_bidi_g_0, x, h_bidi_g_bueno)
-axis([600, 1300, 8, 11.5])
+xline(1000, '--', 'LineWidth', 1.8)
+rectangle('Position',[790 8.2 410 2.6],'EdgeColor','r', 'LineWidth', 1.8)
+axis([700, 1250, 8, 11.25])
 grid on
 title('Bidirectional 2D-2D, t=20')
 xlabel('x')
 ylabel('height')
-legend('Gradient = 0', 'Gradient \neq 0');
+legend('Gradient = 0', 'Gradient \neq 0', 'domain division');
 saveas(gcf,'bidirectional_graph.png')
 
 
